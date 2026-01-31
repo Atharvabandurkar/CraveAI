4bd88a86ca0afc61b9235fb48d86a1251e94dd49
Atharva <Atharvabandurkar8@gmail.com>
2026-01-31 23:01:22 +0530

    Implement Header  with authentication buttons for signed-in and signed-out users

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,8 +1,21 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="fixed top-0 w-full border-b border-stone-200 bg-stone-50/80 backdrop-blur-md z-50 supports-backdrop-filter:bg-stone-50/60">
+        <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+    </header>
   )
 }
 
