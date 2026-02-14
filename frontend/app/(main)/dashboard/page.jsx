a492629ef2e54cc58120c1e461f6d19ab5e2a349
Atharva <Atharvabandurkar8@gmail.com>
2026-02-14 17:03:47 +0530

    Add meal fetching actions and create Dashboard and MainLayout components

---
+++ frontend/app/(main)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div className='min-h-screen px-4 py-12 bg-background text-foreground'>
+      DashboardPage
+    </div>
+  )
+}
+
+export default DashboardPage
